7c4d9083186cf0735a0e55cbc68c20d90eba5a64
Alexander Martinz <eviscerationls@gmail.com>
2015-09-09 18:07:01 -0700

    SystemUI: On-The-Go Mode (1/2)
    Julian Veit <claymore1298@gmail.com>: update for A16
    
    @neobuddy89: Updated for A12
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ia5fa1650254dd30da9a67cae2e053cc0e5cf7cb4
    
     Conflicts:
    	packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
    	packages/SettingsProvider/src/android/provider/settings/validators/SystemSettingsValidators.java
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialogLite.java
    
    Change-Id: I635f36e0cfcd6fab9b6186bd4bf525b8c519de9d

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -132,7 +132,12 @@ public class SystemSettings {
                 Settings.System.CV_PREFERRED_INTENSITY,
                 Settings.System.PEAK_REFRESH_RATE,
                 Settings.System.MIN_REFRESH_RATE,
-                Settings.System.ACCESSIBILITY_FORCE_INVERT_COLOR_OVERRIDE_PACKAGES_TO_DISABLE
+                Settings.System.ACCESSIBILITY_FORCE_INVERT_COLOR_OVERRIDE_PACKAGES_TO_DISABLE,
+                // AICP Settings start
+                Settings.System.GLOBAL_ACTIONS_ONTHEGO,
+                Settings.System.ON_THE_GO_ALPHA,
+                Settings.System.ON_THE_GO_SERVICE_RESTART,
+                Settings.System.ON_THE_GO_CAMERA
         ));
         return settings.toArray(new String[0]);
     }

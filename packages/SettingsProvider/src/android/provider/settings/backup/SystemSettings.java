ffa56cd77f55d20a37e51066e093702a47f2061b
calebcabob <calphonic@gmail.com>
2021-01-06 15:50:57 +0300

    base: LockscreenCharging: Bring in °F/°C toggle [1/2]
    Extracted from
    https://review.aosip.dev/c/AOSIP/platform_frameworks_base/+/16594
    
    Default updated to celcius
    
    Change-Id: If618c5235d4864c8e68d6f266d1012f3ba64ba3b

--- packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
+++ packages/SettingsProvider/src/android/provider/settings/backup/SystemSettings.java
@@ -142,7 +142,8 @@ public class SystemSettings {
                 Settings.System.TRANSPARENT_POWER_MENU,
                 Settings.System.TRANSPARENT_POWER_DIALOG_DIM,
                 Settings.System.POWER_MENU_ANIMATIONS,
-                Settings.System.LOCKSCREEN_BATTERY_INFO
+                Settings.System.LOCKSCREEN_BATTERY_INFO,
+                Settings.System.LOCKSCREEN_BATTERY_INFO_TEMP_UNIT
         ));
         return settings.toArray(new String[0]);
     }

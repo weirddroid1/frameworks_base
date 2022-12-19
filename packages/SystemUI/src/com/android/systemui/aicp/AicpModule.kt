34aab3d988473e6cbe9a079d45bb6e04817a0378
ShevT <evgeny.shishkov@gmail.com>
2022-12-19 16:24:06 +0300

    SystemUI: Add Screenshot tile
        OmniROM:
    
        From: maxwen <max.weninger@gmail.com>
        Date: Wed, 14 Oct 2020 17:17:31 +0200
        Subject: [PATCH] SystemUI: qs tiles revenge
    
        -screenshot
    
        [micky387]
         * edit to the new A12 Tiles API
         * Add secondaryLabel for the long press screenshot tile
    
        From: micky387 <mickaelsaibi@free.fr>
        Date: Wed, 14 Dec 2022 13:12:26 +0100
        Subject: [PATCH] SystemUI: Update ScreenshotTile after merge
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
+++ packages/SystemUI/src/com/android/systemui/aicp/AicpModule.kt
@@ -36,6 +36,7 @@ import com.android.systemui.qs.tiles.ProfilesTile
 import com.android.systemui.qs.tiles.ReadingModeTile
 import com.android.systemui.qs.tiles.RefreshRateTile
 import com.android.systemui.qs.tiles.SmartPixelsTile
+import com.android.systemui.qs.tiles.ScreenshotTile
 import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.VolumeTile
@@ -148,6 +149,12 @@ interface AicpModule {
     @IntoMap
     @StringKey(SmartPixelsTile.TILE_SPEC)
     fun bindSmartPixelsTile(smartPixelsTile: SmartPixelsTile): QSTileImpl<*>
+
+    /** Inject ScreenshotTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(ScreenshotTile.TILE_SPEC)
+    fun bindScreenshotTile(screenshotTile: ScreenshotTile): QSTileImpl<*>
 
     /** Inject SyncTile into tileMap in QSModule */
     @Binds
@@ -435,6 +442,19 @@ interface AicpModule {
                     ),
                 instanceId = uiEventLogger.getNewInstanceId(),
                 category = TileCategory.DISPLAY,
+            )
+
+        @StringKey(ScreenshotTile.TILE_SPEC)
+        fun provideScreenshotTile(uiEventLogger: QsEventLogger): QSTileConfig =
+            QSTileConfig(
+                tileSpec = TileSpec.create(ScreenshotTile.TILE_SPEC),
+                uiConfig =
+                    QSTileUIConfig.Resource(
+                        iconRes = com.android.internal.R.drawable.ic_screenshot,
+                        labelRes = com.android.internal.R.string.global_action_screenshot
+                    ),
+                instanceId = uiEventLogger.getNewInstanceId(),
+                category = TileCategory.UTILITIES,
             )
 
         @Provides

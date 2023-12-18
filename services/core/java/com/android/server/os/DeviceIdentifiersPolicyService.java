362a4c22a8599e311272801ab72a8b8db189d18d
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 14:54:30 +0200

    define READ_DEVICE_SERIAL_NUMBER permission for Build.getSerial() access
    Build.getSerial() is protected with the very broad READ_PRIVILEGED_PHONE_STATE permission.
    
    Android Auto needs Build.getSerial() in some cases, but doesn't need most of the other privileges
    that READ_PRIVILEGED_PHONE_STATE grants.

--- services/core/java/com/android/server/os/DeviceIdentifiersPolicyService.java
+++ services/core/java/com/android/server/os/DeviceIdentifiersPolicyService.java
@@ -16,6 +16,7 @@
 
 package com.android.server.os;
 
+import android.Manifest;
 import android.annotation.NonNull;
 import android.annotation.Nullable;
 import android.content.Context;
@@ -26,6 +27,7 @@ import android.os.IDeviceIdentifiersPolicyService;
 import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.util.Slog;
 
 import com.android.internal.telephony.TelephonyPermissions;
 import com.android.server.SystemService;
@@ -74,9 +76,19 @@ public final class DeviceIdentifiersPolicyService extends SystemService {
                                 + Binder.getCallingUid());
             }
 
-            if (!TelephonyPermissions.checkCallingOrSelfReadDeviceIdentifiers(mContext,
-                    callingPackage, callingFeatureId, "getSerial")) {
-                return Build.UNKNOWN;
+            try {
+                if (!TelephonyPermissions.checkCallingOrSelfReadDeviceIdentifiers(mContext,
+                        callingPackage, callingFeatureId, "getSerial")) {
+                    return Build.UNKNOWN;
+                }
+            } catch (SecurityException e) {
+                String perm = Manifest.permission.READ_DEVICE_SERIAL_NUMBER;
+                if (mContext.checkCallingPermission(perm) == PackageManager.PERMISSION_GRANTED) {
+                    Slog.d(DeviceIdentifiersPolicyService.class.getSimpleName(),
+                            callingPackage + " has " + perm + ", allowed serial number access");
+                } else {
+                    throw e;
+                }
             }
             return SystemProperties.get("ro.serialno", Build.UNKNOWN);
         }

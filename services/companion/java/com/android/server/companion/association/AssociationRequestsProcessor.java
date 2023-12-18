c076c33212bbe4197a02fc00c8772da1e4147d36
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-12-18 19:28:43 +0200

    aauto: support allowing association with "automotive projection" devices

--- services/companion/java/com/android/server/companion/association/AssociationRequestsProcessor.java
+++ services/companion/java/com/android/server/companion/association/AssociationRequestsProcessor.java
@@ -44,6 +44,7 @@ import static com.android.server.companion.utils.RolesUtils.isRolelessProfile;
 import static com.android.server.companion.utils.Utils.generateRandom128BitKey;
 import static com.android.server.companion.utils.Utils.prepareForIpc;
 
+import static com.android.server.pm.ext.AndroidAutoHooks.isAndroidAutoWithGrantedBasePrivPerms;
 import static java.util.Objects.requireNonNull;
 
 import android.annotation.NonNull;
@@ -185,11 +186,15 @@ public class AssociationRequestsProcessor {
         enforcePermissionForCreatingAssociation(mContext, request, packageUid);
         enforceUsesCompanionDeviceFeature(mContext, userId, packageName);
 
+        final boolean shouldSkipAddRoleHolderCheck =
+                AssociationRequest.DEVICE_PROFILE_AUTOMOTIVE_PROJECTION.equals(request.getDeviceProfile())
+                        && isAndroidAutoWithGrantedBasePrivPerms(packageName, userId);
+
         // 2a. Check if association can be created without launching UI (i.e. CDM needs NEITHER
         // to perform discovery NOR to collect user consent).
         if (request.isSelfManaged() && !request.isForceConfirmation()
                 && !DEVICE_PROFILES_WITH_REQUIRED_CONFIRMATION.contains(request.getDeviceProfile())
-                && !willAddRoleHolder(request, packageName, userId)) {
+                && (shouldSkipAddRoleHolderCheck || !willAddRoleHolder(request, packageName, userId))) {
             // 2a.1. Create association right away.
             createAssociationAndNotifyApplication(request, packageName, userId,
                     /* macAddress */ null, callback, /* resultReceiver */ null);
@@ -342,6 +347,11 @@ public class AssociationRequestsProcessor {
                         .setPackagesToNotify(null)
                         .setMetadata(new PersistableBundle())
                         .build();
+
+        if (!skipRoleGrant) {
+            skipRoleGrant = AssociationRequest.DEVICE_PROFILE_AUTOMOTIVE_PROJECTION.equals(deviceProfile)
+                    && isAndroidAutoWithGrantedBasePrivPerms(packageName, userId);
+        }
 
         if (skipRoleGrant) {
             Slog.i(TAG, "Created association for " + association.getDeviceProfile() + " and userId="

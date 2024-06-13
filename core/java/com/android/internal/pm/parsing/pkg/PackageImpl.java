b696fb94b282951ca620c9843eeb8455cbd711eb
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-06-13 18:03:51 +0300

    fix ApplicationInfo flag initialization being skipped in some cases

--- core/java/com/android/internal/pm/parsing/pkg/PackageImpl.java
+++ core/java/com/android/internal/pm/parsing/pkg/PackageImpl.java
@@ -2801,6 +2801,7 @@ public class PackageImpl implements ParsedPackage, AndroidPackageInternal,
     @Override
     public PackageImpl hideAsParsed() {
         assignDerivedFields();
+        assignDerivedFields2();
         return this;
     }
 

c0b66db8f1ff8f344eb3e0c39b334825f2a9aa62
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-10-07 14:54:03 +0300

    add Bundle.toStringDeep()

--- core/java/android/os/Bundle.java
+++ core/java/android/os/Bundle.java
@@ -1589,6 +1589,16 @@ public final class Bundle extends BaseBundle implements Cloneable, Parcelable {
     }
 
     /**
+     * Same as {@link #toString()}, but unparcels the internal map if it isn't unparcelled already,
+     * which ensures that the internal map contents are included in the result.
+     * @hide
+     */
+    public String toStringDeep() {
+        unparcel((mFlags & FLAG_DEFUSABLE) != 0);
+        return toString();
+    }
+
+    /**
      * @hide
      */
     public synchronized String toShortString() {

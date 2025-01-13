0b1136207bdecf7b093f9816d5e67c162acd7e2a
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2025-01-13 18:25:03 +0200

    make Context.getUser() part of system API
    It's useful for APIs that require specifying the user explicitly.

--- core/java/android/content/Context.java
+++ core/java/android/content/Context.java
@@ -8033,7 +8033,7 @@ public abstract class Context {
      * @hide
      */
     @NonNull
-    @SystemApi(client = SystemApi.Client.MODULE_LIBRARIES)
+    @SystemApi
     @TestApi
     @RavenwoodSupported(type = SupportType.SUBCLASS, subclass = "ContextImpl")
     public @CanBeALL @CanBeCURRENT UserHandle getUser() {

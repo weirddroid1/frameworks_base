3f1d26b8c02574bf0319939a7e32cc16944b07e9
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-05-27 15:39:03 +0000

    Definition of AppInfoExt flag for apps signed using Play signing

--- core/java/android/ext/AppInfoExtFlag.java
+++ core/java/android/ext/AppInfoExtFlag.java
@@ -10,10 +10,12 @@ import java.lang.annotation.RetentionPolicy;
 @SystemApi
 public interface AppInfoExtFlag {
     /* SysApi */ int HAS_GMSCORE_CLIENT_LIBRARY = 0;
+    /** @hide */ int HAS_PLAY_STORE_SOURCE_STAMP_ON_APK_CERTS = 1;
 
     /** @hide */
     @IntDef(value = {
             HAS_GMSCORE_CLIENT_LIBRARY,
+            HAS_PLAY_STORE_SOURCE_STAMP_ON_APK_CERTS,
     })
     @Retention(RetentionPolicy.SOURCE)
     @interface Enum {}

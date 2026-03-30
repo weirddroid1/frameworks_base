734bee5d4e098d2889d39babcdec81f35d845781
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2026-03-30 07:00:23 +0000

    gmscompat: remove GmsCorePackageFlag from system API

--- core/java/android/app/compat/gms/GmsCorePackageFlag.java
+++ core/java/android/app/compat/gms/GmsCorePackageFlag.java
@@ -1,9 +1,6 @@
 package android.app.compat.gms;
 
-import android.annotation.SystemApi;
-
 /** @hide */
-@SystemApi
 public interface GmsCorePackageFlag {
     int GRANT_PERMS_FOR_ICC_AUTHENTICATION = 0;
 }
